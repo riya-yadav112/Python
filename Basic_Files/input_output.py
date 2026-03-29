40b16586e3f4057230c4d7ff814c8c1025088333
Bhupesh Jain <bhupeshjain@desktop2-2.local>
2026-03-29 20:53:51 +0530

    variables

---
+++ Basic_Files/input_output.py
@@ -0,0 +1,34 @@
+# print("Enter a value here:\n")
+
+# n = input()
+# b = input()
+
+# n = int(n)
+# b = int(b)
+
+# print("Total number is -\n")
+# print(n+b)
+
+
+# n = input('What is your name\n')
+# print("My name is:", n)   // " ", ' ' , "  ""  
+
+
+# print("Python ", end='n')
+
+# print("28","03", "2026", sep='-')
+
+
+# <<<<<<<< PYTHON FORMAT CONVERSATION RULES >>>>>>>>>>>>>>>>>>
+
+# Roll_No = 203221034
+# Name = "Riya"
+# Height = 5.743
+
+# print("Stuent Name is %s, her Roll No is %d and Height is %f " %(Name, Roll_No, Height))
+
+a = 22
+b = 7
+c = a/b
+
+print(f'Division of{a} and {b} is {c}')
---
+++ README.md
@@ -0,0 +1,3 @@
+## Python Restart:
+
+- test file:
